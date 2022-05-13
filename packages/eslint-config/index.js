0fb12f219ff9da481a061e6bd5dc0507fcdc260a
Rasmus Nørskov <rasmus@norskov.org>
2022-05-13 02:49:44 +0200

    update

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,45 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest"],
+  root: true,
+  env: { jest: true, node: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  ignorePatterns: ["jest.config.js"],
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
 };
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,13 +1,6 @@
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
-  useTabs: true,
-  semi: true,
-  singleQuote: false,
-  quoteProps: "as-needed",
-  jsxSingleQuote: false,
-  trailingComma: "es5",
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: "avoid",
+  useTabs: false,
+  endOfLine: "lf",
 };
